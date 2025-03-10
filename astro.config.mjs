03a7c31915ca093907a1814e1fb6af29ff80dff8
Chas Nelson <github@chasnelson.co.uk>
2025-03-10 17:30:28 +0000

    fix: resolve runtime error around bg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://chasnelson1990.github.io/",
   base: "fjelltopp-astro",
@@ -11,11 +10,11 @@ export default defineConfig({
     locales: ["en", "fr"],
     defaultLocale: "en",
     routing: {
-        prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
