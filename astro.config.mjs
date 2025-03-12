800272072314b4fe9cea7abd3963f323c2822c61
Chas Nelson <github@chasnelson.co.uk>
2025-03-12 15:30:15 +0000

    feat: add broken link checker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import astroBrokenLinksChecker from "astro-broken-link-checker";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -20,5 +21,11 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    astroBrokenLinksChecker({
+      logFilePath: "broken-links.log",
+      checkExternalLinks: false, // Optional: check external links (currently, caching to disk is not supported, and it is slow )
+    }),
+  ],
 });
